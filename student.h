45c8c0e3fcb90312cccdac78be4b60256f1fe202
Matas <matas.remeskevicius@gmail.com>
2020-04-26 16:17:27 +0300

    tried to make a base and derived class

--- student.h
+++ student.h
@@ -4,42 +4,48 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-class student
+//base class
+class Stud_base
 {
-private:
-    std::string name;
-    std::string surname;
-    std::vector<int> homework;
-    int exam;
-    double finalGrade;
-    bool random;
-public:
-    //base constructors
-    student()
-    {
-    }
-    student(std::string n, std::string s): name(n), surname(s) {}
-    //getters
-    std::string getName() {return name;}
-    std::string getSurname() {return surname;}
-    std::vector<int> getGrades() {return homework;}
-    int getGradesCount() {return homework.size(); }
-    int getExam() {return exam;}
-    bool getRandom() {return random;}
-    double getFinal() {return finalGrade;}
-    bool hasGrades() {return !homework.empty();}
-    //setters
-    void setName(int n) {name = n;}
-    void setSurname (int s) {surname = s;}
-    void addGrade(int grade) {homework.push_back(grade); }
-    void setGrades(std::vector<int> grades) {homework = grades; }
-    void setExam(int examg) {exam = examg; }
-    void setRandom(bool reiksme) {random = reiksme; }
-    void setFinal(double f) {finalGrade = f;}
-    //operator
-    student& operator=(const student& studd);
-    //destructor
-    ~student();
+    public: 
+        Stud_base(std::string n, std::string s) : name(n), surname(s) {}
+    protected:
+        std::string name;
+        std::string surname;
+        std::string id;
+        
+};
+class student : public Stud_base
+{
+    private:
+        std::vector<int> homework;
+        int exam;
+        double finalGrade;
+        bool random;
+    public:
+        //base constructor
+        student(std::string n, std::string s) : Stud_base(n,s) {}
+        //getters
+        std::string getName() {return name;}
+        std::string getSurname() {return surname;}
+        std::vector<int> getGrades() {return homework;}
+        int getGradesCount() {return homework.size(); }
+        int getExam() {return exam;}
+        bool getRandom() {return random;}
+        double getFinal() {return finalGrade;}
+        bool hasGrades() {return !homework.empty();}
+        //setters
+        void setName(std::string n) {name = n;}
+        void setSurname (std::string s) {surname = s;}
+        void addGrade(int grade) {homework.push_back(grade); }
+        void setGrades(std::vector<int> grades) {homework = grades; }
+        void setExam(int examg) {exam = examg; }
+        void setRandom(bool reiksme) {random = reiksme; }
+        void setFinal(double f) {finalGrade = f;}
+        //operator
+        student& operator=(const student& studd);
+        //destructor
+    ~student(); 
 };
 
 #endif // STUDENT_H
